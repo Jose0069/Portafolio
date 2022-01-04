df25038308e0f2c750021f523f92b96b5aec8d37
Jose0069 <94661449+Jose0069@users.noreply.github.com>
2022-01-04 01:57:50 -0400

    Add files via upload

--- scripts/header.js
+++ scripts/header.js
@@ -1,4 +1,26 @@
 const header = document.getElementById('header');
-header.lastElementChild.addEventListener('click', () => {
-    header.children[1].classList.toggle('header__menu--show');
-})
+header.firstElementChild.lastElementChild.addEventListener('click', () => {
+    header.firstElementChild.children[1].classList.toggle('header__menu--show');
+});
+const main = document.getElementById('main');
+const sections = Array.from(main.children);
+const headerLinks = Array.from(header.firstElementChild.children[1].children);
+const indicatorOfSection = (entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            const actualSection = Array.from(headerLinks).find(link => link.dataset.url == entry.target.id);
+            actualSection.classList.add('header__button--active');
+            for(const link of headerLinks){
+                if(link != actualSection){
+                    link.classList.remove('header__button--active');
+                }
+            }
+        }
+    });
+}
+const observer = new IntersectionObserver(indicatorOfSection, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.8
+});
+sections.forEach(section => observer.observe(section));
